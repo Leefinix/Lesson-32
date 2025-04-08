a1c67639e5476b579765201ed40375bd804cc65d
Leefinix <joaquinnleo@gmail.com>
2025-04-08 19:05:06 +0700

    q

---
+++ quiz.py
@@ -0,0 +1,25 @@
+import random
+
+class FruitQuiz:
+
+    def __init__(self):
+        self.fruits={'apple':'red', 'orange':'orange', 'watermelon':'green', 'banana':'yellow'}
+
+    def quiz(self):
+        while(True):
+            fruit, color = random.choice(list(self.fruits.items()))
+
+            print("What is the color the {}".format(fruit))
+            user_answer = input()
+            if(user_answer.lower() == color):
+                print("Correct Answer!")
+            else:
+                print("Wrong Answer!")
+
+            option = int(input("Enter 0, if you want to play again otherwise enter 1: "))
+            if (option):
+                break
+
+print("Welcome to the Fruit Quiz!")
+fq = FruitQuiz()
+fq.quiz()
